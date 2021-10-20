f7a1fa20c2b32d4020760301523d89d75e76398b
jamiecm17 <moffajamie@gmail.com>
2021-10-20 12:43:30 -0500

    Establish tests for 'neighbor' function

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,29 @@ using TDD
 using Test
 
 @testset "TDD.jl" begin
-    # Write your tests here.
+    graph = [
+        [2,3],
+        [1],
+        [1],
+        [5],
+        [4]]
+    @test neighbor(1)==[1,2,3]
+    @test neighbor(4)==[4,5]
+
+    graph = [
+        [2],
+        [1],
+        [1],
+        [5],
+        [4]]
+    @test neighbor(1)==[1,2]
+    @test neighbor(3)==[3,1]
+
+    graph = [
+        [2],
+        [1],
+        [1],
+        [],
+        [4]]
+    @test neighbor(4)==[4]
 end
